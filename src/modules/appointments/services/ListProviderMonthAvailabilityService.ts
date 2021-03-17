d062a6898c72272357ade04fcafe4d703da7363d
Vinicius Santos <vinicius.vph@gmail.com>
2021-03-17 13:51:31 -0300

    list of available times

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,10 +1,7 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 
 import IAppointmentRepository from '../repositories/IAppointmentsRepository'
-import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-
-import User from '@modules/users/infra/typeorm/entities/User';
-
 
 interface IRequest {
   provider_id: string;
@@ -30,8 +27,26 @@ class ListProviderMonthAvailabilityService {
       year,
       month,
     })
-    
-    return [{day: 1, available: false}]
+
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (value, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
   }
 }
 
